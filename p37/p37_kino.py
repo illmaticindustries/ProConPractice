d04ec7bcb42d3a61661d37e42d258083bfbc845e
Yohei Kinowaki <kinowaki.y@gmail.com>
2015-11-01 02:24:02 +0900

    change find_next

--- p37/p37_kino.py
+++ p37/p37_kino.py
@@ -35,45 +35,42 @@ def find_next(count,que,p,maze_input):
 	p_l = [p[0],p[1]-1]
 	p_r = [p[0],p[1]+1]
 	que.append(p)
-	if p_u[0] > 0:
-		if maze_input[p_u[0]][p_u[1]] != "#" and not find_que(que,p_u):
-			find_next(count+1,que,p_u,maze_input)
+	if p_u[0] >= 0:           
 		if maze_input[p_u[0]][p_u[1]] == "G":
 			solve.append(count + 1)
+			print que
+			return
+		if maze_input[p_u[0]][p_u[1]] != "#" and not find_que(que,p_u):
+			find_next(count+1,que,p_u,maze_input)
 	if p_d[0] < N:
-		if maze_input[p_d[0]][p_d[1]] != "#" and not find_que(que,p_d):
-			find_next(count+1,que,p_d,maze_input)
 		if maze_input[p_d[0]][p_d[1]] == "G":
 			solve.append(count + 1)
-	if p_l[1] > 0:
-		if maze_input[p_l[0]][p_l[1]] != "#" and not find_que(que,p_l):
-			find_next(count+1,que,p_l,maze_input)
+			print que
+			return
+		if maze_input[p_d[0]][p_d[1]] != "#" and not find_que(que,p_d):
+			find_next(count+1,que,p_d,maze_input)
+	if p_l[1] >= 0:
 		if maze_input[p_l[0]][p_l[1]] == "G":
 			solve.append(count + 1)
+			print que
+			return
+		if maze_input[p_l[0]][p_l[1]] != "#" and not find_que(que,p_l):
+			find_next(count+1,que,p_l,maze_input)
 	if p_r[1] < M:
-		if maze_input[p_r[0]][p_r[1]] != "#" and not find_que(que,p_r):
-			find_next(count+1,que,p_r,maze_input)
 		if maze_input[p_r[0]][p_r[1]] == "G":
 			solve.append(count + 1)
+			print que
+			return
+		if maze_input[p_r[0]][p_r[1]] != "#" and not find_que(que,p_r):
+			find_next(count+1,que,p_r,maze_input)
+		
 	return
 
-def test():
-	global N,M,solve
-	solve = []
-	f_N,f_M,maze_input = read_input("p37.in")
-	N = f_N
-	M = f_M
-	print N,M,maze_input
-	n,m = find_start(maze_input)
-	p0 = [n,m]
-	q0 = []
-	find_next(0,q0,p0,maze_input)
-	print solve
 
-def test2():
+def test(file_name):
 	global N,M,solve
 	solve = []
-	f_N,f_M,maze_input = read_input("p37_2.in")
+	f_N,f_M,maze_input = read_input(file_name)
 	N = f_N
 	M = f_M
 	print N,M,maze_input
@@ -84,5 +81,7 @@ def test2():
 	print solve
 
 
-test()
-test2()
+test("p37.in")
+test("p37_2.in")
+test("p37_3.in")
+test("p37_4.in")
