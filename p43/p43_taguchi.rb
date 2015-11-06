34a2593ac6d809d14b67b6b637d14aeed2f1c500
Tetsuya Taguchi <bouzu.ao@gmail.com>
2015-11-07 01:52:11 +0900

    comment

--- p43/p43_taguchi.rb
+++ p43/p43_taguchi.rb
@@ -43,31 +43,31 @@ def jobs(file)
   first_end_time_index = @end_times.find_index(@end_times.min)
   start_times = @start_times[first_end_time_index..-1]
   end_times = @end_times.last(start_times.size)
-  p 'first jobs'
-  p jobs << @num - start_times.size + 1
+  # 'first jobs'
+  jobs << @num - start_times.size + 1
 
   loop do
     # スタートの配列を終了時間に合わせる
-    p start_times = start_times.select { |time| time > end_times.first }
-    p end_times = end_times.last(start_times.size)
+    start_times = start_times.select { |time| time > end_times.first }
+    end_times = end_times.last(start_times.size)
 
     # スタートの位置を終了が一番速い箇所に設定する
-    p 'end_times_index'
-    p end_times_index = end_times.find_index(end_times.min)
-    p 'start_times'
-    p start_times = start_times[end_times_index..-1]
-    p 'jobs'
-    p jobs << @num - start_times.size + 1
-    p 'end_times'
-    p end_times = end_times.last(start_times.size)
-    p '---'
+    # 'end_times_index'
+    end_times_index = end_times.find_index(end_times.min)
+    # 'start_times'
+    start_times = start_times[end_times_index..-1]
+    # 'jobs'
+    jobs << @num - start_times.size + 1
+    # 'end_times'
+    end_times = end_times.last(start_times.size)
+    # '---'
     break if end_times.empty? || start_times.select { |time| time > end_times.first }.empty?
   end
 
   p "count: #{jobs.size}, jobs: #{jobs}"
 end
 
-# jobs('p43.in')
-# jobs('p43_1.in')
-# jobs('p43_2.in')
+jobs('p43.in')
+jobs('p43_1.in')
+jobs('p43_2.in')
 jobs('p43_3.in')
