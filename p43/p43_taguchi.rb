22320f745085fd13c52647cfafd6556d09ebcb72
Tetsuya Taguchi <bouzu.ao@gmail.com>
2015-11-07 01:47:22 +0900

    sort

--- p43/p43_taguchi.rb
+++ p43/p43_taguchi.rb
@@ -7,8 +7,26 @@ def read_file(file_name)
   end
 
   @num = array.first
-  @start_times = array[1..@num.to_i]
-  @end_times = array[(@num.to_i + 1)..-1]
+  p 'start_times'
+  p start_times = array[1..@num.to_i]
+  p 'end_times'
+  p end_times = array[(@num.to_i + 1)..-1]
+  times = []
+  (0..(@num.to_i - 1)).each do |num|
+    times[num] = [start_times[num], end_times[num]]
+  end
+
+  # end_timesでソートする
+  p times = times.sort { |a, b| a[1] <=> b[1] }
+
+  # 戻す
+  @start_times = []
+  @end_times = []
+
+  times.each do |time|
+    @start_times << time[0]
+    @end_times  << time[1]
+  end
 end
 
 # read_file('p43_1.in')
@@ -17,6 +35,9 @@ end
 def jobs(file)
   read_file(file)
   jobs = []
+
+  # 終了時間が早い順にソートする
+  end_times = @end_times.sort
 
   # スタートの位置を終了が一番速い箇所に設定する
   first_end_time_index = @end_times.find_index(@end_times.min)
@@ -43,9 +64,10 @@ def jobs(file)
     break if end_times.empty? || start_times.select { |time| time > end_times.first }.empty?
   end
 
-  p "jobs: #{jobs}"
+  p "count: #{jobs.size}, jobs: #{jobs}"
 end
 
 # jobs('p43.in')
 # jobs('p43_1.in')
-jobs('p43_2.in')
+# jobs('p43_2.in')
+jobs('p43_3.in')
