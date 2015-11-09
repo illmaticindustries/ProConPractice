cddc04fe6e6742aafba9e0cefd0a360a58a50d00
Kosuke Yamagishi <kosuke1012@gmail.com>
2015-11-09 23:09:04 +0900

    yamapy ver2

--- p45/p45_yama.py
+++ p45/p45_yama.py
@@ -21,17 +21,18 @@ if __name__ == '__main__':
 	
 	for j in range(N-1):
 		if(S[0] < S[len(S)-1]):
-			if(S[0]<T[0]):
+			if(S[0]<=T[0]):
 				T.insert(0,S[0])
 				S.pop(0)
 			else:
 				T.append(S[0])
 				S.pop(0)
 		else:
-			if(S[len(S)-1]<T[0]):
+			if(S[len(S)-1]<=T[0]):
 				T.insert(0,S[len(S)-1])
 				S.pop(len(S)-1)
 			else:
 				T.append(S[len(S)-1])
 				S.pop(len(S)-1)
+		print T
 	print T
