8dc59b3fef2491d8fdb582d663a8cea48ee1953e
kinowaki <kinowaki.y@gmail.com>
2015-11-15 22:25:02 +0900

    add explain

--- p47/p47_kino.py
+++ p47/p47_kino.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+# not correct answer
 
 def read_input(file_name):
     f = open(file_name,'r')
@@ -11,11 +12,15 @@ def read_input(file_name):
     return N,R,X
 
 def calc(N,R,X):
+    """
+    L : maximum length in X
+    O : length for covering N nodes 
+    """
     L = X[-1]-X[0]
     O = R*(N-1)
     M = (L/R)-1
     if L > O:
-        return N
+        return N 
     elif M > N:
         return N -1
     else:
@@ -29,3 +34,4 @@ def test(file_name):
 test('p47.in')
 test('p47_1.in')
 test('p47_2.in')
+test('p47_3.in')
