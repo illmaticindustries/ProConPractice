91e175c8b14b0f3677d126104ed819a101db02cb
Kosuke Yamagishi <kosuke1012@gmail.com>
2015-11-15 21:27:32 +0900

    yamapy ver 1

---
+++ p47/p47_yama.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+import sys
+
+if __name__ == '__main__':
+	#input	
+	X = []
+	C = []
+	ans = 0
+	N = int(raw_input())	
+	R = int(raw_input())	
+	for i in range(N):
+		X.append(int(raw_input()))
+	
+	X.sort()
+
+	for j in range(N):
+		if(j==0 or X[j-1] < (X[j] - R)):
+			if(j == N-1 or (X[j+1] > (X[j] + R))):
+				if(X[j] not in C):
+					C.append(X[j])
+					ans += 1
+			else:
+				C.append(X[j+1])
+				ans += 1
+		elif(X[j-1] not in C):
+			if(X[j] not in C):
+				C.append(X[j])
+				ans += 1
+	
+	print ans
+	print C
