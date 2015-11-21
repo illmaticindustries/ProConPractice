7467821c5374b7bab99b8d202e69d4ccf411b73c
kinowaki <kinowaki.y@gmail.com>
2015-11-22 01:19:36 +0900

    add solve

--- p49/p49_kino.py
+++ p49/p49_kino.py
@@ -7,11 +7,26 @@ def read_input(file_name):
     for l in f:
 	L.append(int(l.rstrip('\n')))
     f.close()
+    L.sort()
     return N,L
+
+def solve(N,L):
+    sum = 0
+    #in case len(L) > 2
+    while len(L) > 2:
+	for l in L:
+	    sum = sum + l
+	L.pop()
+    
+    #in case len(L) = 2
+    for l in L:
+	sum = sum + l
+    return sum
 
 def test(file_name):
     N,L = read_input(file_name)
     print N,L
+    print solve(N,L)
 
 test('p49.in')
 
