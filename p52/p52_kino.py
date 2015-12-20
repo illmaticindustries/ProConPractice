8d69dce6b970e5a01ea628c02ba98f7b0532f7c2
Yohei Kinowaki <kinowaki.y@gmail.com>
2015-12-21 00:44:22 +0900

    paste from text

--- p52/p52_kino.py
+++ p52/p52_kino.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+dp = []
 
 def read_input(file_name):
     f = open(file_name)
@@ -6,7 +7,7 @@ def read_input(file_name):
     W = int(f.readline())
     w_v =[]
     for l in f:
-        l = l.rstrip('\n').split(',')l
+        l = l.rstrip('\n').split(',')
         w = int(l[0])
         v = int(l[1])
         w_v.append([w,v])
@@ -14,7 +15,34 @@ def read_input(file_name):
 
     return N,W,w_v
 
+def rec(i,j,N,W,w_v):
+    global dp
+
+    if dp[i][j] >= 0:
+        return dp[i][j]
+
+    res = 0
+    if i == N:
+        res = 0
+    elif j < w_v[i][0]:
+        res = rec(i+1,j,N,W,w_v)
+    else:
+        res = max(rec(i+1,j,N,W,w_v), rec(i+1,j-w_v[i][0],N,W,w_v)+w_v[i][1])
+    dp[i][j] = res
+    return dp[i][j]
+
 def test(file_name):
-    print read_input(file_name)
+    N,W,w_v = read_input(file_name)
+    global dp
+    dp_n = []
+    dp_w = -1
+    for n in range(N+1):
+        for w in range(W+1):
+            dp_n.append(dp_w)
+        dp.append(dp_n)
+        dp_n = []
+    print rec(0,W,N,W,w_v)
+
+    
 
 test('p52.in')
