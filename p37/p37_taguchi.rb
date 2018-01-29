23cc086686efddaaf9191ff61c1a06e36c96b8d4
Tetsuya Taguchi <bouzu.ao@gmail.com>
2018-01-29 16:43:19 +0900

    add: p37 Ruby

--- p37/p37_taguchi.rb
+++ p37/p37_taguchi.rb
@@ -1,79 +1,24 @@
-class ShortestPathMaze
-  def initialize
-    @field = []
-    @start = {}
-    @goal = {}
-    line_num = 0
-
-    File.open('p37.in') do |file|
-      file.each_line do |line|
-        if line.include?('S')
-          @start[:x] = line_num
-          @start[:y] = line.index('S')
-        end
-
-        @field << line.chomp.split('')
-        line_num += 1
-      end
+array = []
+File.open('./p37_4.in') { |file| file.each_line { |x| array << x.chomp } }
+n, m, maze = array[0].to_i, array[1].to_i, array[2..-1]
+start, goal = {}, {}
+# Get start/goal Coord
+maze.each_with_index { |m, i|  [['S', start], ['G', goal]].each { |h| h[1][:x], h[1][:y] = i, m.index(h[0]) if m.include?(h[0]) } }
+INF = 10000
+d = Array.new(n).map { Array.new(m, INF) } 
+d[start[:x]][start[:y]] = 0
+q = []
+q.push [start[:x], start[:y]]
+dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
+while q.any? do
+  current = q.pop
+  break if current[0] == goal[:x] && current[1] == goal[:y]
+  for i in 0..3
+    nx, ny = current[0] + dx[i], current[1] + dy[i]
+    if 0 <= nx && nx < n && 0 <= ny && ny < m && maze[nx][ny] != '#' && d[nx][ny] == INF 
+      q.push [nx, ny]
+      d[nx][ny] = d[current[0]][current[1]] + 1
     end
-
-    @height = @field.size
-    @width = @field.first.size
-    @count = 0
-    @queue = []
-    @next_field_queue = []
-  end
-
-  # 現在地を更新する
-  def current_field(y, x)
-    p "current_field: (#{x}, #{y})"
-    # p "@next_field_queue -= [[x, y]]"
-    @next_field_queue -= [[x, y]]
-
-    # p @queue << [x, y]
-
-    @field[y][x] = '#'
-    direction = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-
-    direction.each do |dy, dx|
-      # p "dx: #{dx}, dy: #{dy}"
-      nx = x + dx
-      ny = y + dy
-
-      # p "nx: #{nx}, ny: #{ny}"
-      next if nx >= @height || ny >= @width
-      # p "next_field: #{@field[ny][nx]}"
-
-      if 0 <= nx && nx < @height && 0 <= ny && ny < @width && @field[ny][nx] == '.'
-        # @count += 1
-        p "add #{[nx, ny]}"
-        @next_field_queue << [nx, ny]
-        # p "@next_field_queue: #{@next_field_queue}"
-      end
-
-      if 0 <= nx && nx < @height && 0 <= ny && @field[ny][nx] == 'G'
-        # p "nx: #{nx}, ny:#{ny}"
-        # @count += 1
-        p "Goal! #{@queue.size}"
-      end
-    end
-
-    next_field_queue
-  end
-
-  def solve
-    current_field(@start[:x], @start[:y])
-  end
-
-  def next_field_queue
-    p "next_field_queue: #{@next_field_queue}"
-
-    @next_field_queue.each do |nx, ny|
-      current_field(ny, nx)
-    end
-
   end
 end
-
-shortest_path_maze = ShortestPathMaze.new
-shortest_path_maze.solve
+p d[goal[:x]][goal[:y]]
