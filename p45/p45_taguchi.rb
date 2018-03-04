e8f7746c49180a933a5df86bf9aad7c0936965f7
Tetsuya Taguchi <bouzu.ao@gmail.com>
2018-03-04 09:24:14 +0900

    add: p45 Ruby

--- p45/p45_taguchi.rb
+++ p45/p45_taguchi.rb
@@ -1,39 +1,14 @@
-def read_file(file_name)
-  array = []
-  File.open("p45/#{file_name}") do |file|
-    file.each_line do |line|
-      array << line.chomp
-    end
+n, s = 0, ''
+File.open(ARGV[0]) { |file| file.each_with_index { |line, i| i == 0 ? n = line.chomp : s = line.chomp } }
+reverse_s, t = s.reverse, ''
+while !s.empty? do
+  if s < reverse_s
+    t << s.slice!(0)
+    reverse_s.chop!
+  else
+    t << reverse_s.slice!(0)
+    s.chop!
   end
-  @n = array[0]
-  @s = array[1]
 end
+p t
 
-def jobs(file)
-  read_file(file)
-  s = @s
-  reverse_s = @s.reverse
-  t = ''
-  # p "s: #{s}"
-
-  loop do
-    if s < reverse_s
-      t << s.slice!(0)
-      reverse_s.chop!
-    else
-      t << reverse_s.slice!(0)
-      s.chop!
-    end
-    # p "t: #{t}"
-    # p '---'
-    # p "s: #{s}"
-    break if s.empty?
-  end
-
-  p t
-end
-
-jobs('p45.in')
-jobs('p45_1.in')
-jobs('p45_2.in')
-jobs('p45_3.in')
