ba6a9266c938d0aefa9717f95aea6c165b3960ed
Tetsuya Taguchi <bouzu.ao@gmail.com>
2018-04-05 14:22:39 +0900

    add: p52

--- p52/p52_taguchi.rb
+++ p52/p52_taguchi.rb
@@ -1,64 +1,23 @@
-def read_file(file_name)
-  array = []
-  File.open("p52/#{file_name}") do |file|
-    file.each_line do |line|
-      array << line.chomp
-    end
-  end
-  p '@element_count='
-  p @element_count = array[0].to_i
-  p 'max_weight='
-  p @max_weight = array[1].to_i
-  p 'array='
-  @array = []
-  p @array = array[2..-1].map { |pair| pair.split(',').map { |a| a.to_i } }
-end
-
-# 現在の価値
-# @param 現在の要素の番号, 重さの許容量
-def value(n, max_weight_now)
-
-  @value ||= []
-  @value[n] ||= []
-  # メモに存在する場合はメモの内容を返す
-  return @value[n][max_weight_now] unless @value[n].nil? || @value[n][max_weight_now].nil?
-
-  # 現在のnが要素数を超える場合は終わり
-  if n == @element_count
-    value = 0
+n, max_weight, weights, values = 0, 0, [], []
+File.open(ARGV[0]) { |file| file.each_with_index do |line, i| 
+  if i == 0 
+    n = line.chomp.to_i
+  elsif i == 1
+    max_weight = line.chomp.to_i
   else
-    # p "#{n}番目"
-    n_weight = @array[n][0]
-    n_value = @array[n][1]
+    a = line.chomp.split(',')
+    weights << a[0].to_i
+    values << a[1].to_i
+  end
+end }
 
-    # 現在の要素の重さが重さの許容量を超える場合は、自分自身は入れないで次の要素に行く
-    if n_weight > max_weight_now
-      value = value(n + 1, max_weight_now)
-
-    # 自分自身を入れる場合と入れない場合の最大値を比較する
-    else
-      value = [
-        # 自分自身を入れない場合
-        value(n + 1, max_weight_now),
-        # 自分自身を入れる場合
-        value(n + 1, max_weight_now - n_weight) + n_value
-      ].max
+value_table = Array.new(n + 1).map { Array.new(max_weight + 1, 0) } 
+for i in 0..(n - 1) do
+  for w in 0..max_weight 
+    value_table[i + 1][w] = [value_table[i + 1][w], value_table[i][w]].max
+    if w + weights[i] <= max_weight
+      value_table[i + 1][w + weights[i]] = [value_table[i + 1][w + weights[i]], value_table[i][w] + values[i]].max
     end
   end
-
-  # メモに値を代入する
-  return @value[n][max_weight_now] = value
 end
-
-# 実行メソッド
-def execute(file_name)
-  read_file(file_name)
-  # 価値を求める
-  # 0番目の要素の場合、重さの許容量は@max_weightになる
-  p '合計値: ' + value(0, @max_weight).to_s
-end
-
-execute('p52.in')
-# execute('p52_1.in')
-# execute('p52_2.in')
-# execute('p52_3.in')
+p value_table[n][max_weight]
